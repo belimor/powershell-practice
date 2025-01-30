67af6cceba927ccecedc1e72f68e314ec20469a6
Dmitry <dmytro.vaga@gmail.com>
2025-01-29 17:27:40 -0700

    initial commit

---
+++ script.ps1
@@ -0,0 +1,17 @@
+# Powershell Script
+
+# Print message to the console:
+Write-Host "Hello World!"
+
+# Variables
+$name = "John"
+$age = 25
+
+Write-Host "Hello, my name is $name and I am $age years old"
+
+# Math operations
+$a
+$b
+$sum = $a + $b
+
+Write-Host "Sum is $sum"
